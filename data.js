3c462c1b0e03169ecb8a71138428a31885a2e2fe
Joyce Anne Remo <joyceanneremo05@gmail.com>
2025-11-26 23:02:54 +0800

    new

---
+++ data.js
@@ -0,0 +1,78 @@
+var users = [
+    
+    {
+        "year": 2015,
+        "title": "Born to Shine",
+        "picture": "images/2015.jpg",
+        "desc": "Seventeen shines as a self-producing rookie group under Pledis Entertainment."
+    },
+    {
+        "year": 2016,
+        "title": "Growing Strong",
+        "picture": "images/2016.jpg",
+        "desc": "Gaining recognition with heartfelt tracks and charming performances."
+
+    },
+    {
+        "year": 2017,
+        "title": "Fearless Youth",
+        "picture": "images/2017.jpg",
+        "desc": "Teenage energy with bold concepts and powerful choreography."
+
+    },
+    {
+        "year": 2018,
+        "title": "Frames of Joy",
+        "picture": "images/2018.jpg",
+        "desc": "Expanding fandom globally with cinematic storytelling in music."
+
+    },
+    {
+        "year": 2019,
+        "title": "Echoes of Emotion",
+        "picture": "images/2019.jpg",
+        "desc": "Emotional depth where Seventeen explores mature themes and artistry."
+
+    },
+    {
+        "year": 2020,
+        "title": "Together and Always",
+        "picture": "images/2020.jpg",
+        "desc": "Resilient youth with breaking records and uniting CARATs."
+    },
+        {
+        "year": 2021,
+        "title": "Passion Unstoped",
+        "picture": "images/2021.jpg",
+        "desc": "Global recognition  first major world tours and international hits."
+    },
+    {
+        "year": 2022,
+        "title": "Rise and Radiate",
+        "picture": "images/2022.jpg",
+        "desc": "Healing and joy: music that comforts and celebrates fans."
+
+    },
+    {
+        "year": 2023,
+        "title": "Healing and Celebration",
+        "picture": "images/2023.jpg",
+        "desc": "Masterful performance, refined artistry, stage presence that captivates fans worldwide."
+
+    },
+    {
+        "year": 2024,
+        "title": "Crafted to Lead",
+        "picture": "images/2024.jpg",
+        "desc": "Every song and choreography this year reflects the precision and passion of the group."
+
+    },
+    {
+        "year": 2025,
+        "title": "A Decade in Diamond",
+        "picture": "images/2025.jpg",
+        "desc": "A decade of diamonds and teamwork, a decade full of music and memories."
+    }
+
+
+];
