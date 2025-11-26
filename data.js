6c97f48968836ee57d156a0e2e061a90e0237f06
Joyce Anne Remo <joyceanneremo05@gmail.com>
2025-11-27 01:34:54 +0800

    new

--- data.js
+++ data.js
@@ -1,78 +1,143 @@
 var users = [
-    
+
     {
         "year": 2015,
         "title": "Born to Shine",
-        "picture": "images/2015.jpg",
+        "picture": "assets/2015.jpg",
         "desc": "Seventeen shines as a self-producing rookie group under Pledis Entertainment."
     },
     {
         "year": 2016,
         "title": "Growing Strong",
-        "picture": "images/2016.jpg",
+        "picture": "assets/2016.jpg",
         "desc": "Gaining recognition with heartfelt tracks and charming performances."
 
     },
     {
         "year": 2017,
         "title": "Fearless Youth",
-        "picture": "images/2017.jpg",
+        "picture": "assets/2017.jpg",
         "desc": "Teenage energy with bold concepts and powerful choreography."
 
     },
     {
         "year": 2018,
         "title": "Frames of Joy",
-        "picture": "images/2018.jpg",
+        "picture": "assets/2018.jpg",
         "desc": "Expanding fandom globally with cinematic storytelling in music."
 
     },
     {
         "year": 2019,
         "title": "Echoes of Emotion",
-        "picture": "images/2019.jpg",
+        "picture": "assets/2019.jpg",
         "desc": "Emotional depth where Seventeen explores mature themes and artistry."
 
     },
     {
         "year": 2020,
         "title": "Together and Always",
-        "picture": "images/2020.jpg",
+        "picture": "assets/2020.jpg",
         "desc": "Resilient youth with breaking records and uniting CARATs."
     },
-        {
+    {
         "year": 2021,
         "title": "Passion Unstoped",
-        "picture": "images/2021.jpg",
+        "picture": "assets/2021.jpg",
         "desc": "Global recognition  first major world tours and international hits."
     },
     {
         "year": 2022,
         "title": "Rise and Radiate",
-        "picture": "images/2022.jpg",
+        "picture": "assets/2022.jpg",
         "desc": "Healing and joy: music that comforts and celebrates fans."
 
     },
     {
         "year": 2023,
         "title": "Healing and Celebration",
-        "picture": "images/2023.jpg",
+        "picture": "assets/2023.jpg",
         "desc": "Masterful performance, refined artistry, stage presence that captivates fans worldwide."
 
     },
     {
         "year": 2024,
         "title": "Crafted to Lead",
-        "picture": "images/2024.jpg",
+        "picture": "assets/2024.jpg",
         "desc": "Every song and choreography this year reflects the precision and passion of the group."
 
     },
     {
         "year": 2025,
         "title": "A Decade in Diamond",
-        "picture": "images/2025.jpg",
+        "picture": "assets/2025.jpg",
         "desc": "A decade of diamonds and teamwork, a decade full of music and memories."
     }
 
 
+];
+
+var members = [
+
+    {
+        "picture": "assets/member1.jpg",
+        "name": "Choi Seungchol", 
+        "position": "Leader, Rapper"
+    },
+    { 
+        "picture": "assets/member2.jpg", 
+        "name": "Yoon Jeonghan",
+        "position": "Lead Vocalist"
+    },
+    { 
+        "picture": "assets/member3.jpg", 
+        "name": "Joshua Hong",
+        "position": "Sub Vocalist" 
+    },
+    { 
+        "picture": "assets/member4.jpg", 
+        "name": "Moon Junhui",
+        "position": "Lead Dancer" 
+    },
+    { 
+        "picture": "assets/member5.jpg", 
+        "name": "Kwon Soonyoung",
+        "position": "Main Dancer" 
+    },
+    { 
+        "picture": "assets/member6.jpg", 
+        "name": "Jeon Wonwoo",
+        "position":"Rapper" 
+    },
+    { 
+        "picture": "assets/member7.jpg", 
+        "name": "Lee Jihoon",
+        "position": "Lead Vocalist, Producer" 
+    },
+    { 
+        "picture": "assets/member8.jpg", 
+        "name": "Lee Seokmin",
+        "position": "Main Vocalist" 
+    },
+    { 
+        "picture": "assets/member9.jpg", 
+        "name": "Kim Mingyu",
+        "position": "Rapper" 
+    },
+    { 
+        "picture": "assets/member10.jpg", 
+        "name": "Seo Myungho",
+        "position":"Lead Dancer" 
+    },
+    { 
+        "picture": "assets/member11.jpg", 
+        "name": "Boo Seungkwan",
+        "position": "Main Vocalist"
+    },
+    { 
+        "picture": "assets/member13.jpg", 
+        "name": "Lee Dino and Vernon",
+        "position": "Dancer and Rapper"    
+    },
+    
 ];
