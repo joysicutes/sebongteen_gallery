7a600a0b53844478b0a2a76efc7d23d0e1e91517
Joyce Anne Remo <joyceanneremo05@gmail.com>
2025-11-27 21:37:19 +0800

    new

--- data.js
+++ data.js
@@ -81,63 +81,329 @@ var members = [
 
     {
         "picture": "assets/member1.jpg",
-        "name": "Choi Seungchol", 
+        "name": "Choi Seungchol",
         "position": "Leader, Rapper"
     },
-    { 
-        "picture": "assets/member2.jpg", 
+    {
+        "picture": "assets/member2.jpg",
         "name": "Yoon Jeonghan",
         "position": "Lead Vocalist"
     },
-    { 
-        "picture": "assets/member3.jpg", 
+    {
+        "picture": "assets/member3.jpg",
         "name": "Joshua Hong",
-        "position": "Sub Vocalist" 
+        "position": "Sub Vocalist"
     },
-    { 
-        "picture": "assets/member4.jpg", 
+    {
+        "picture": "assets/member4.jpg",
         "name": "Moon Junhui",
-        "position": "Lead Dancer" 
+        "position": "Lead Dancer"
     },
-    { 
-        "picture": "assets/member5.jpg", 
+    {
+        "picture": "assets/member5.jpg",
         "name": "Kwon Soonyoung",
-        "position": "Main Dancer" 
+        "position": "Main Dancer"
     },
-    { 
-        "picture": "assets/member6.jpg", 
+    {
+        "picture": "assets/member6.jpg",
         "name": "Jeon Wonwoo",
-        "position":"Rapper" 
+        "position": "Rapper"
     },
-    { 
-        "picture": "assets/member7.jpg", 
+    {
+        "picture": "assets/member7.jpg",
         "name": "Lee Jihoon",
-        "position": "Lead Vocalist, Producer" 
+        "position": "Lead Vocalist, Producer"
     },
-    { 
-        "picture": "assets/member8.jpg", 
+    {
+        "picture": "assets/member8.jpg",
         "name": "Lee Seokmin",
-        "position": "Main Vocalist" 
+        "position": "Main Vocalist"
     },
-    { 
-        "picture": "assets/member9.jpg", 
+    {
+        "picture": "assets/member9.jpg",
         "name": "Kim Mingyu",
-        "position": "Rapper" 
+        "position": "Rapper"
     },
-    { 
-        "picture": "assets/member10.jpg", 
+    {
+        "picture": "assets/member10.jpg",
         "name": "Seo Myungho",
-        "position":"Lead Dancer" 
+        "position": "Lead Dancer"
     },
-    { 
-        "picture": "assets/member11.jpg", 
+    {
+        "picture": "assets/member11.jpg",
         "name": "Boo Seungkwan",
         "position": "Main Vocalist"
     },
-    { 
-        "picture": "assets/member13.jpg", 
+    {
+        "picture": "assets/member13.jpg",
         "name": "Lee Dino and Vernon",
-        "position": "Dancer and Rapper"    
+        "position": "Dancer and Rapper"
     },
-    
+
+];
+
+var albumsData = [
+
+    {
+        "era": "assets/era_1.webp",
+        "album_era_title": "Love & Letter",
+        "album_era_date": "April 25, 2016",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kJzXYUQYnAckZwnVjjoMSAPjR1BHNLP7Y"
+    },
+    {
+        "era": "assets/repack_1.webp",
+        "album_era_title": "Love & Letter Repackage",
+        "album_era_date": "April 25, 2016",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kJzXYUQYnAckZwnVjjoMSAPjR1BHNLP7Y"
+    },
+
+    {
+        "era": "assets/era_2.webp",
+        "album_era_title": "Teen,Age",
+        "album_era_date": "November 6, 2017",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_n1ss_dZ7ANz8PzJWSDpYH5UBimBXwmdF4"
+    },
+    {
+        "era": "assets/era_6.webp",
+        "album_era_title": "Director's Cut",
+        "album_era_date": "February 5, 2018",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nO-n3P4UOHgp_odTz1Ye_jUurk68Sa_F8"
+    },
+    {
+        "era": "assets/era_3.webp",
+        "album_era_title": "An Ode",
+        "album_era_date": "September 16, 2019",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mVS5lM__bZsmGVYhrH5e9Mr6WlMWW3eT4"
+    },
+    {
+        "era": "assets/era_7.webp",
+        "album_era_title": "Semicolon",
+        "album_era_date": "October 19,2020",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mqZKTQ9i5yunhO4jiiU1jyCLSLQPxnftI"
+    },
+    {
+        "era": "assets/era_4.webp",
+        "album_era_title": "Face the Sun",
+        "album_era_date": "May 27, 2022",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nbTL0noUZU7rqEKwcvPuDPqeoOP-17to0"
+    },
+    {
+        "era": "assets/repack_2.webp",
+        "album_era_title": "Sector 17",
+        "album_era_date": "July 18 2022",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nbTL0noUZU7rqEKwcvPuDPqeoOP-17to0"
+    },
+    {
+        "era": "assets/era_5.webp",
+        "album_era_title": "Happy Burstday",
+        "album_era_date": "May 26, 2025",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kh7Eq_1nus733fKxANQJzMP96GxAEkvYA"
+    }
+]
+
+var miniSinglesData = [
+    {
+        "era": "assets/single1.jpg",
+        "album_era_title": "Mini Single 1",
+        "album_era_date": "Jan 15, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_example1"
+    },
+    {
+        "era": "assets/single2.jpg",
+        "album_era_title": "Mini Single 2",
+        "album_era_date": "Feb 20, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_example2"
+    },
+    {
+        "era": "assets/single3.jpg",
+        "album_era_title": "Mini Single 3",
+        "album_era_date": "Mar 10, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_example3"
+    },
+    {
+        "era": "assets/single4.jpg",
+        "album_era_title": "Mini Single 4",
+        "album_era_date": "Apr 05, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_example4"
+    },
+    {
+        "era": "assets/single5.jpg",
+        "album_era_title": "Mini Single 5",
+        "album_era_date": "May 18, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_example5"
+    },
+    {
+        "era": "assets/single6.jpg",
+        "album_era_title": "Mini Single 6",
+        "album_era_date": "Jun 12, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_example6"
+    }
+];
+
+
+var single = [
+
+    {
+        "img": "assets/single2.jpg",
+        "title": "17 Carat",
+        "single_date": "May 29, 2015",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mAW5btibJsNLpAsC7zcvJpTa2AvB0MciU"
+    },
+    {
+        "img": "assets/single3.jpg",
+        "title": "Boys Be",
+        "single_date": "September 10, 2015",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mK9X43PoCU9dR13om6OwdDwrVw_zPY2eI"
+    },
+    {
+        "img": "assets/single4.jpg",
+        "title": "Going Seventeen",
+        "single_date": "December 5, 2016",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_ncMXAqVXQkMXWIkcLNQHPQ32tbbbR8KSk"
+    },
+    {
+        "img": "assets/single5.jpg",
+        "title": "Al1",
+        "single_date": "May 22, 2017",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nd5qT5LhWHBSd_43yT04JQZpreoemW6YY"
+    },
+    {
+        "img": "assets/single6.jpg",
+        "title": "You Make My Day",
+        "single_date": "July 16, 2018",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nFwbVclzCdhHJNsKSoI1wx5pSkN6ur-K4"
+    }
+    ,
+    {
+        "img": "assets/single7.jpg",
+        "title": "You Made My Dawn",
+        "single_date": "January 21, 2019",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_lulj3zHN1CzWeSHyxrevlkDkGsbKbh1bs"
+    }
+    ,
+    {
+        "img": "assets/single8.jpg",
+        "title": "Heng:garae",
+        "single_date": "June 22, 2020",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_ndwdShCF_8gJC3d3PFit5F1gS2jo0lBcg"
+    }
+    ,
+    {
+        "img": "assets/single9.jpg",
+        "title": "Your Choice",
+        "single_date": "June 18, 2021",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_n9oj3kWJc9C1_XKtNd1tKlNWmICBcgc_M"
+    }
+    ,
+    {
+        "img": "assets/single10.jpg",
+        "title": "Attaca",
+        "single_date": "October 22, 2021",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kpQw0PiIDxSEjm-RmMw26T3E7j65Iv03E"
+    }
+    ,
+    {
+        "img": "assets/single11.jpg",
+        "title": "FML",
+        "single_date": "April 25, 2023",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mnzRiVfYORPvHbwvpmN9TROQE2naGS6sg"
+    }
+    ,
+    {
+        "img": "assets/single12.jpg",
+        "title": "Seventeenth Heaven",
+        "single_date": "October 23, 2023",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kAahFEN7TNN7_rheS6VRIiDA4N5lLR6qY"
+    },
+    {
+        "img": "assets/single13.jpg",
+        "title": "Spill the Feels",
+        "single_date": "October 14, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_lEUOtMw7S_xsFNqWxxAS9Q8MVt3HTr-6Q"
+    }
+
+];
+
+
+var single = [
+
+    {
+        "img": "assets/single_2.webp",
+        "title": "17 Carat",
+        "single_date": "May 29, 2015",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mAW5btibJsNLpAsC7zcvJpTa2AvB0MciU"
+    },
+    {
+        "img": "assets/single_3.webp",
+        "title": "Boys Be",
+        "single_date": "September 10, 2015",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mK9X43PoCU9dR13om6OwdDwrVw_zPY2eI"
+    },
+    {
+        "img": "assets/single_4.webp",
+        "title": "Going Seventeen",
+        "single_date": "December 5, 2016",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_ncMXAqVXQkMXWIkcLNQHPQ32tbbbR8KSk"
+    },
+    {
+        "img": "assets/single_5.webp",
+        "title": "Al1",
+        "single_date": "May 22, 2017",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nd5qT5LhWHBSd_43yT04JQZpreoemW6YY"
+    },
+    {
+        "img": "assets/single_6.webp",
+        "title": "You Make My Day",
+        "single_date": "July 16, 2018",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_nFwbVclzCdhHJNsKSoI1wx5pSkN6ur-K4"
+    }
+    ,
+    {
+        "img": "assets/single_7.webp",
+        "title": "You Made My Dawn",
+        "single_date": "January 21, 2019",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_lulj3zHN1CzWeSHyxrevlkDkGsbKbh1bs"
+    }
+    ,
+    {
+        "img": "assets/single_8.webp",
+        "title": "Heng:garae",
+        "single_date": "June 22, 2020",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_ndwdShCF_8gJC3d3PFit5F1gS2jo0lBcg"
+    }
+    ,
+    {
+        "img": "assets/single_9.webp",
+        "title": "Your Choice",
+        "single_date": "June 18, 2021",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_n9oj3kWJc9C1_XKtNd1tKlNWmICBcgc_M"
+    }
+    ,
+    {
+        "img": "assets/single_10.webp",
+        "title": "Attaca",
+        "single_date": "October 22, 2021",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kpQw0PiIDxSEjm-RmMw26T3E7j65Iv03E"
+    }
+    ,
+    {
+        "img": "assets/single_11.webp",
+        "title": "FML",
+        "single_date": "April 25, 2023",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_mnzRiVfYORPvHbwvpmN9TROQE2naGS6sg"
+    }
+    ,
+    {
+        "img": "assets/single-12.webp",
+        "title": "Seventeenth Heaven",
+        "single_date": "October 23, 2023",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_kAahFEN7TNN7_rheS6VRIiDA4N5lLR6qY"
+    },
+    {
+        "img": "assets/single_13.webp",
+        "title": "Spill the Feels",
+        "single_date": "October 14, 2024",
+        "link": "https://music.youtube.com/playlist?list=OLAK5uy_lEUOtMw7S_xsFNqWxxAS9Q8MVt3HTr-6Q"
+    }
+
 ];
